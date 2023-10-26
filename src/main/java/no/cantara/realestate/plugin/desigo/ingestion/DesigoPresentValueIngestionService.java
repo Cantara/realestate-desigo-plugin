576b2c435a2f42e6049816a5f2098e8ae8ce3fd6
baardl <bard.lind@gmail.com>
2023-10-26 19:12:13 +0200

    Moved code into this project.

--- src/main/java/no/cantara/realestate/plugin/desigo/ingestion/DesigoPresentValueIngestionService.java
+++ src/main/java/no/cantara/realestate/plugin/desigo/ingestion/DesigoPresentValueIngestionService.java
@@ -79,7 +79,7 @@ public class DesigoPresentValueIngestionService implements PresentValueIngestion
     @Override
     public boolean initialize(PluginConfig config) {
         this.config = config;
-        String apiUrl = config.asString("sd.api.uri", "http://localhost:8080");
+            String apiUrl = config.asString("sd.api.uri", "http://<localhost>:<port>");
         apiUri = URI.create(apiUrl);
         desigoApiClient = new DesigoApiClientRest(apiUri);
         isInitialized = true;
